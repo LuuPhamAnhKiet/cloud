8b9fe0f3aea6d52281ef8d28e16244cbc003bb8e
kiet <kietlpagcc200083@fpt.edu.vn>
2022-10-21 13:33:44 +0700

    ok

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php 
+// $conn = mysqli_connect('localhost', 'root', '','tune_source')
+// or die("Can not connect database".mysqli_connect_error());
+?><?php
+$conn = pg_connect("postgres://eqsswfxaphixgx:79c5717c086ecfdb796105255f2a0f48e0ec8b5c2278c2e680b24bb8689abc65@ec2-54-145-139-208.compute-1.amazonaws.com:5432/d200jeu77acrei");
+	echo 'Connected Successfully!!!';
+	if(!$conn)
+	{
+		die("Could not connect to database");
+    }
+    ?>
