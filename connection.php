34edb4d031541161e9e8e264aa808252165ad121
kiet <kietlpagcc200083@fpt.edu.vn>
2022-10-22 11:33:08 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-$conn = pg_connect("
-postgres://lgfcaixytznaft:6283dea978507943cc48135a3241ba1018c7f99066844d14342c9639a0c4dbfb@ec2-54-147-36-107.compute-1.amazonaws.com:5432/deogegs8stb7jn");
+$conn = pg_connect("postgres://lgfcaixytznaft:6283dea978507943cc48135a3241ba1018c7f99066844d14342c9639a0c4dbfb@ec2-54-147-36-107.compute-1.amazonaws.com:5432/deogegs8stb7j");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
